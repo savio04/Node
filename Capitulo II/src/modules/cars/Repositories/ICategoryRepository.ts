eeea25eda6bc87c809faf93b0839a58d856eb1f0
savio04 <savio27.05.2001@gmail.com>
2021-04-20 19:31:09 -0300

    delete cap II

--- Capitulo II/src/modules/cars/Repositories/ICategoryRepository.ts
+++
@@ -1,9 +0,0 @@
-import Category from '../model/Category'
-
-interface ICategoryRepository{
-    findByName(name:string):Category
-    seeAll():Category[]
-    create(name:string, descirption:string,created_at:Date):Category
-}
-
-export default ICategoryRepository
--- Capitulo II/src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import categoriesRoutes from './category.routes'
-import SpacificationRoute from './spacification.routes'
-const Routes = express.Router()
-
-Routes.use("/categories", categoriesRoutes)
-Routes.use("/spacification", SpacificationRoute)
-
-export default Routes
