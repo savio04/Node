c277306fe1ecccb3ed9091b149ec21efbd3d457b
savio04 <savio27.05.2001@gmail.com>
2021-05-20 09:14:16 -0300

    add stream reading files

--- Capitulo II/src/modules/cars/useCases/Importcategory/ImportCatgoryUseCase.ts
+++ Capitulo II/src/modules/cars/useCases/Importcategory/ImportCatgoryUseCase.ts
@@ -1,8 +1,17 @@
-
+import fs from 'fs'
+import csvParse from 'csv-parse'
 
 class ImportCategoryUseCase{
     execute(file:Express.Multer.File){
-        console.log(file)
+        const stream = fs.createReadStream(file.path)
+
+        const ParseFile = csvParse()
+
+        stream.pipe(ParseFile)
+
+        ParseFile.on('data', (line) => {
+            console.log(line)
+        })
     }
 }
 
