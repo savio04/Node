9db421aa6aab91c8f224c7def02cc7468dc29898
savio04 <savio27.05.2001@gmail.com>
2021-05-20 20:38:42 -0300

    add documatation

--- Desafios/Desafio-01---Introdu-o-ao-SOLID/src/index.ts
+++ Desafios/Desafio-01---Introdu-o-ao-SOLID/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
-
+import swagger from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile))
 
 app.use("/users", usersRoutes);
 
