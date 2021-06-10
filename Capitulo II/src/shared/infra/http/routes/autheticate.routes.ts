177c245e0e7c8d9cc69ecf3240e83e1e4fdaf2ab
savio04 <savio27.05.2001@gmail.com>
2021-06-10 10:32:37 -0300

    Rename directory for rentx

--- Capitulo II/src/shared/infra/http/routes/autheticate.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import AutheticateController from '../../../../modules/accounts/UseCases/AutheticateUser/AutheticateContoller'
-
-const AutheticateRoute = Router()
-
-const AuthContoller = new AutheticateController
-AutheticateRoute.post('/',AuthContoller.handle)
-
-export default AutheticateRoute
--- Capitulo II/src/shared/infra/http/routes/spacification.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import CreateSpecificationController from '../../../../modules/cars/useCases/CreateSpecification/CreateSpecificationController'
-
-const SpecificationRoute = express.Router()
-
-const specificationController = new CreateSpecificationController
-SpecificationRoute.post('/',specificationController.handle)
-export default SpecificationRoute
