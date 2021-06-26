6e07889b27a78ebd9cc1cba5c97175275be5c5aa
savio04 <savio27.05.2001@gmail.com>
2021-06-26 09:29:53 -0300

    first integration test

--- Rentx/src/shared/infra/typeorm/index.ts
+++ Rentx/src/shared/infra/typeorm/index.ts
@@ -1,22 +1,13 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
-
-// interface IOptions {
-//   host: string;
-// }
-
-// getConnectionOptions().then(options => {
-//   const newOptions = options as IOptions;
-//   newOptions.host = 'database_ignite'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-//   createConnection({
-//     ...options,
-//   });
-// });
+import { createConnection, getConnectionOptions } from 'typeorm'
 
 export default async (host = 'database_ignite') => {
     const defaultOptions = await getConnectionOptions()
     return createConnection(
         Object.assign(defaultOptions, {
-            host
+            host,
+            database:process.env.NODE_ENV === 'test' 
+            ? "rentex_test"
+            : defaultOptions.database
         })
     )
 }
