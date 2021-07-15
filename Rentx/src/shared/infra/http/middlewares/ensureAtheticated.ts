e9c9747351c308b9786d62c83f3cdfc4c3d051ef
savio04 <savio27.05.2001@gmail.com>
2021-07-15 11:34:52 -0300

    controller refresh token

--- Rentx/src/shared/infra/http/middlewares/ensureAtheticated.ts
+++ Rentx/src/shared/infra/http/middlewares/ensureAtheticated.ts
@@ -2,22 +2,30 @@ import { Request,Response,NextFunction } from 'express'
 import { verify } from 'jsonwebtoken'
 import AppError from '../../../errors/AppError'
 import UserRepository from '../../../../modules/accounts/infra/typeorm/repositories/UserRepository'
+import UserTokenRepository from '../../../../modules/accounts/infra/typeorm/repositories/UserTokenRepository'
+import auth from '../../../../config/auth'
 
 interface IPayload{
     sub:string
 }
 async function ensureAutheticaded(request:Request,response:Response,next:NextFunction){
     const {authorization} = request.headers
+    const userTokenRepository = new UserTokenRepository
     if(!authorization){
         throw new AppError('token not exist',401)
     }
 
     const [,token] = authorization.split(' ')
     try{
-        const {sub:user_id} = verify(token, "a367595574905e7f68deba4d753a8e3b") as IPayload
+        const {sub:user_id} = verify(
+            token,
+            auth.secret_refresh_token
+        ) as IPayload
     
-        const userRepository = new UserRepository
-        const user = await userRepository.findById(user_id)
+        const user = await userTokenRepository.findByUserIdAndRefreshToken(
+            user_id,
+            token
+        )
         if(!user){
             throw new AppError('user not existing',401)
         }
--- Rentx/src/shared/infra/http/routes/autheticate.routes.ts
+++ Rentx/src/shared/infra/http/routes/autheticate.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 import AutheticateController from '../../../../modules/accounts/UseCases/AutheticateUser/AutheticateContoller'
+import RefreshTokenController from '../../../../modules/accounts/UseCases/RefreshToken/RefreshTokenController'
 
 const AutheticateRoute = Router()
 
 const AuthContoller = new AutheticateController
+const refreshTokenController = new RefreshTokenController
+
 AutheticateRoute.post('/',AuthContoller.handle)
+
+AutheticateRoute.post('/refresh-token', refreshTokenController.handle)
 
 export default AutheticateRoute
