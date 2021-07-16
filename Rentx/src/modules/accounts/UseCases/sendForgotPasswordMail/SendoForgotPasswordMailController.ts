65a92a7ad15f4c6688ab10508d6435ed2ff8c766
savio04 <savio27.05.2001@gmail.com>
2021-07-16 11:36:44 -0300

    add use cae send forgot password

---
+++ Rentx/src/modules/accounts/UseCases/sendForgotPasswordMail/SendoForgotPasswordMailController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import SendForgotPasswordMailUseCase from "./SendoForgotPasswordMailUseCase";
+
+class SendForgotPasswordMailController{
+    async handle(request:Request,response:Response){
+        const {email} = request.body
+        const sendForgotEmailUseCase = container.resolve(SendForgotPasswordMailUseCase)
+
+        await sendForgotEmailUseCase.execute(email)
+
+        return response.status(200).send()
+    }
+}
+
+export default SendForgotPasswordMailController
--- Rentx/src/shared/container/providers/DateProvider/implemantations/DayjsDateProvider.ts
+++ Rentx/src/shared/container/providers/DateProvider/implemantations/DayjsDateProvider.ts
@@ -34,6 +34,10 @@ class DayJsDateProvider implements IDateProvider{
     addDays(days:number){
         return dayjs().add(days, 'days').toDate()
     }
+
+    addHours(hours:number){
+        return dayjs().add(hours, 'hour').toDate()
+    }
 }
 
 export default DayJsDateProvider
