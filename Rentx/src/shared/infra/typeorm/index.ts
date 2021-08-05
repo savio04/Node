4a67ff2f4087684af22af7ef9c580eeea956a62b
savio04 <savio27.05.2001@gmail.com>
2021-08-05 19:22:16 -0300

    Database docker

--- Rentx/src/shared/infra/typeorm/index.ts
+++ Rentx/src/shared/infra/typeorm/index.ts
@@ -1,10 +1,9 @@
 import { createConnection, getConnectionOptions } from 'typeorm'
 
-export default async (host = 'database_ignite') => {
+export default async () => {
     const defaultOptions = await getConnectionOptions()
     return createConnection(
         Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === 'test' ? 'localhost': host,
             database:process.env.NODE_ENV === 'test' 
             ? "rentex_test"
             : defaultOptions.database
